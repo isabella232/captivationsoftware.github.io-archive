01eb05d35ced0b452fc016f5664a37ebea06d85b
Captivation Software <team@captivationsoftware.com>
2015-12-02 21:44:49 -0500

    New website

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+(function($) {
+  $(document).ready(function() {
+    var $menu = $('.menu').hide();
+    var $hero = $('.hero');
+
+    setInterval(function() {
+      var max = 0;
+      $hero.find('p').each(function(i, p) {
+        var current = $(p).width();
+        if (current > max) max = current;
+      });
+      $hero.width(max);
+    }, 16);
+
+    // Initialize fullpage.js
+    $('#fullpage').fullpage({
+      anchors: ['', 'our-difference', 'solutions', 'capabilities', 'careers', 'contact'],
+      menu: '.menu',
+      loopHorizontal: false,
+      controlArrows: false,
+      verticalCentered: false,
+      responsiveWidth: 700,
+      responsiveHeight: 700
+    });
+
+    // Initialize responsive font sizes
+    $('body').flowtype({
+      minimum: 300,
+      maximum: 1200
+    });
+
+    $('.menu-placeholder').append($menu.show());
+
+    $('.glyphicon-chevron-down').on('click', function() {
+      $.fn.fullpage.moveSectionDown();
+    });
+
+  });
+})(jQuery);
