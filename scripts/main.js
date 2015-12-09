5eef8f23023891c2cf8baef8c9a26d083411eaed
Dave Barbalato <dbarbalato@gmail.com>
2015-12-08 22:02:50 -0500

    should work in ie

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 (function($) {
+
+  // upfront configuration
+  $.support.cors = true;
+
   $(document).ready(function() {
     var templates = {
       menu: $('#menu-template').html(),
@@ -92,7 +96,7 @@
       var $form = $button.closest('form');
       $button.addClass('active').text('Sending...');
       $.ajax({
-          url: "//formspree.io/info@captivationsoftware.com",
+          url: "https://formspree.io/info@captivationsoftware.com",
           method: 'POST',
           data: $form.serialize(),
           dataType: 'json',
