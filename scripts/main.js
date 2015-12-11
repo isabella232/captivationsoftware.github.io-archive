103f4944d8c85239d6065fcb765de2f099ab793f
Dave Barbalato <dbarbalato@gmail.com>
2015-12-10 22:37:36 -0500

    cross browser cors form

--- scripts/main.js
+++ scripts/main.js
@@ -85,30 +85,30 @@
       });
       $form.find('button').prop('disabled', missingRequired);
     });
-
-    // Send email
-    $('form button').on('click', function(e) {
-      var $button = $(e.currentTarget);
-      var $form = $button.closest('form');
-      $button.addClass('active').text('Sending...');
-      $.ajax({
-          url: '//formspree.io/info@captivationsoftware.com',
-          method: 'POST',
-          data: $form.serialize(),
-          dataType: 'json',
-          success: function() {
-            // remove event listeners and update button
-            $form.find(':input').off().val('');
-
-            $button.off('click')
-              .removeClass('error active')
-              .addClass('success')
-              .text('Sent');
-            },
-          error: function() {
-            $button.removeClass('active').addClass('error').text('Try Again');
-          }
-      });
+    //
+    // // Send email
+    // $('form button').on('click', function(e) {
+    //   var $button = $(e.currentTarget);
+    //   var $form = $button.closest('form');
+    //   $button.addClass('active').text('Sending...');
+    //   $.ajax({
+    //       url: '//formspree.io/info@captivationsoftware.com',
+    //       method: 'POST',
+    //       data: $form.serialize(),
+    //       dataType: 'json',
+    //       success: function() {
+    //         // remove event listeners and update button
+    //         $form.find(':input').off().val('');
+    //
+    //         $button.off('click')
+    //           .removeClass('error active')
+    //           .addClass('success')
+    //           .text('Sent');
+    //         },
+    //       error: function() {
+    //         $button.removeClass('active').addClass('error').text('Try Again');
+    //       }
+    //   });
     });
   });
 })(jQuery);
