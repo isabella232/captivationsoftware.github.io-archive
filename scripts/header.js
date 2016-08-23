9ab9da6abbac708f926abb00619626d709836cd9
Dave Barbalato <dbarbalato@gmail.com>
2016-08-23 11:13:18 -0400

    improved header menu

--- scripts/header.js
+++ scripts/header.js
@@ -2,15 +2,41 @@
 
   $(document).ready(function() {
     var $window = $(window);
-    var $toggle = $('.header .nav.btn');
-    var $menu = $('.header .mobile-menu');
+    var $header = $('.header');
+    var $toggle = $header.find('.nav.btn');
+    var $expander = $toggle.find('.expand');
+    var $contracter = $toggle.find('.contract');
+    var $menu = $header.find('.mobile-menu');
+
+    var maxScroll;
+
+    function toggleMenu(isOpen) {
+      $expander.toggleClass('hidden', !isOpen);
+      $contracter.toggleClass('hidden', isOpen);
+
+      maxScroll = !isOpen ? $window.scrollTop() : undefined;
+
+      $header.css({
+        position: isOpen ? 'fixed' : 'absolute',
+        top: isOpen ? 0 : $window.scrollTop()
+      });
+
+      $menu.toggleClass('hidden', isOpen);
+    }
 
     $toggle.on('click', function() {
-      $menu.toggleClass('hidden');
+      var isOpen = $menu.is(':visible');
+
+      toggleMenu(isOpen);
     });
 
     $window.on('resize', function() {
-      $menu.addClass('hidden');
+      toggleMenu(true);
+    });
+
+    $window.on('scroll', function() {
+      if (maxScroll && $window.scrollTop() < maxScroll)
+        window.scrollTo(0, maxScroll);
     });
   });
 })(jQuery);
