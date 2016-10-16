9e0d63ebf358a5e50362d83a3b332b572eeb84e4
Dave Barbalato <dbarbalato@gmail.com>
2016-10-16 00:28:08 -0400

    Refactor

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,21 @@
 (function($) {
+  var $html = $('html');
+  var $body = $('body');
+  var $scroller = $('html,body');
+  var $landing = $body.find('.landing');
+  var $hero = $landing.find('.hero');
+  var $moreButton = $hero.find('.btn');
+  var $moreSection = $landing.next();
 
-  $(document).ready(function() {
-    var $html = $('html');
-    var $body = $('body');
-    var $scroller = $('html,body');
-    var $landing = $body.find('.landing');
-    var $hero = $landing.find('.hero');
-    var $moreButton = $hero.find('.btn');
-    var $moreSection = $landing.next();
-
-    $body.flowtype({
-      minFont: 10,
-      maxFont: 30
-    })
-
-    $moreButton.on('click', function() {
-      $scroller.animate({
-        scrollTop: $moreSection.offset().top - 40
-      });
-    });
-
+  $body.flowtype({
+    minFont: 10,
+    maxFont: 30
   });
+
+  $moreButton.on('click', function() {
+    $scroller.animate({
+      scrollTop: $moreSection.offset().top - 40
+    });
+  });
+
 })(jQuery);
